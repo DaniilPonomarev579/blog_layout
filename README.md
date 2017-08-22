99fb25657930458bc5f1ecf9758b2db8f7d19c6a
Daniil Ponomarev <daniil_ponomarev@epam.com>
2017-08-22 16:08:31 +0300

    Fixed SVG-icons, added build script, divided styles with BEM methodology

---
+++ README.md
@@ -0,0 +1,4 @@
+How can you get a final styles.css file for the page?
+You should run the command "npm run build:css" in your CLI.
+Then you can run server by the command "npm run start" and see
+the blog on 127.0.0.1:8080.
---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server('.');
+var PORT_NUMBER = 8080;
+
+http.createServer(function(req, res) {
+    file.serve(req, res);
+}).listen(PORT_NUMBER);
+
+console.log('Server running on port ' + PORT_NUMBER);
